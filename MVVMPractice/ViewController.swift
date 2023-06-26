e63caa9bf45699aa8e17a25cd4c701f8cf083bd7
dpffpsk <gpljw0419@gmail.com>
2023-06-26 21:10:59 +0900

    진입 화면 생성

--- MVVMPractice/ViewController.swift
+++ MVVMPractice/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let list = ["GoodNews"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = list[indexPath.row]
+        cell.contentConfiguration = content
 
+        return cell
+        
+    }
 }
 
