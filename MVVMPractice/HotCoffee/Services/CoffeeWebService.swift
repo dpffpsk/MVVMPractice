14f199c0a02f21c5fe2cb41339f01c121e08b80e
dpffpsk <gpljw0419@gmail.com>
2023-08-17 16:43:03 +0900

    HotCoffee - URL 통신 구현

---
+++ MVVMPractice/HotCoffee/Services/CoffeeWebService.swift
@@ -0,0 +1,40 @@
+//
+//  CoffeeWebService.swift
+//  MVVMPractice
+//
+//  Created by jiweon.lee on 2023/08/17.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case decodingError
+    case domainError
+    case urlError
+}
+
+struct Resource<T: Codable> {
+    let url: URL
+}
+
+class CoffeeWebService {
+    func load<T>(resource: Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        
+        URLSession.shared.dataTask(with: resource.url) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(.failure(.domainError))
+                return
+            }
+            
+            let result = try? JSONDecoder().decode(T.self, from: data)
+            if let result = result {
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            } else {
+                completion(.failure(.decodingError))
+            }
+            
+        }.resume()
+    }
+}
